236ea6e9af17345f1be574a31b8519f0a639bb8e
Abigail Burtley <abigailtop95@yahoo.com>
2023-07-19 02:25:44 -0400

    add thought process of order in which each function is used, missing function found, missing arrays of options of password length, characters, numbers, and symbols. I will use for loop to re-use code to generate new passwords occording to users preference change.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); //Button to generate password
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function writePassword() {  //Thought process: Second step: Prompts after "click", users options to anonymous password
+  var password = generatePassword(); //Function cannot be found on starter code. Task: make a generatePassword function
+  var passwordText = document.querySelector("#password"); //inspect #password, this will be value of password or new password to show here
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); //Thought process: First step: "event" is click to generate password
