9a2200d5ddd693e222307e14055a399dbaf23d1d
Abigail Burtley <abigailtop95@yahoo.com>
2023-07-19 02:57:06 -0400

    assign uppercase, lowercase, numbers, symbols to each corresponding arrays

--- script.js
+++ script.js
@@ -14,4 +14,7 @@ function writePassword() {  //Thought process: Second step: Prompts after "click
 generateBtn.addEventListener("click", writePassword); //Thought process: First step: "event" is click to generate password
 
 //create my arrays of input:
-const upperCaseLetters: ['A']['B']['C']['D']['E']['F']['G']['H']['I']['J']['K']['L']['M']['N']['O']['P']['Q']['R']['S']['T']['U']['V']['W']['X']['Y']['Z'];
+const upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'];
+const lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','x', 'y', 'z'];
+const symbols = ['~', '!', '@', '#', '$', '%', '*', '^', '&', '/', '+', '?', '_', '-', '<']; 
+const numbers = [0,1,2,3,4,5,6,7,8,9];
